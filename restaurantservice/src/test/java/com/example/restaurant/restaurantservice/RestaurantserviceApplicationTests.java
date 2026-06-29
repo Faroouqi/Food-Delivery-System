b11ec50a154dd1e3cb9c99f6451c06373e6e5a80
NABEEL <urs.nabeel1@gmail.com>
2026-06-29 19:50:41 +0530

    User servce Start

---
+++ restaurantservice/src/test/java/com/example/restaurant/restaurantservice/RestaurantserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant.restaurantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
